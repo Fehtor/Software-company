0b68dafc4eb16605a9a5e8ac6ea90e2c3f193380
Fehtor <f.truhachiov@mail.ru>
2023-10-10 19:16:14 +0300

    14

--- js/calc.js
+++ js/calc.js
@@ -1,17 +1,79 @@
 let nums = document.querySelectorAll("#num-btn")
 let str = document.querySelector("#calculate-string")
+let buffer = ""
+let actionBuffer = ""
 function numClick(event){
     let value = event.currentTarget.getAttribute("value")
     if(value.indexOf("num") != -1){
         let num = value[value.length - 1]
-        console.log(num)
-        str.innerHTML += num;
-
+        if(str.innerHTML.indexOf(".") == -1 && num == "." && str.innerHTML != ""){
+            str.innerHTML += num;
+        }
+        else if(num != "."){
+            str.innerHTML += num;
+        }
+        str.scrollLeft = str.scrollWidth
     }
     else if(value.indexOf("action") != -1){
         let actionType = value.slice(value.indexOf("-") + 1, value.length)
+        if(str.innerHTML != ""){
+            switch(actionType){
+                case "mul":
+                    break;
+                case "minus":
+                    if(buffer == ""){
+                        buffer = str.innerHTML
+                        str.innerHTML = ""
+                        actionBuffer = actionType
+                    }
+                    else if(actionBuffer != ""){
+                        doAction()
+                        actionBuffer = actionType
+
+                    }
+                    break;
+                case "plus":
+                    break;
+                case "equally":
+                    break;
+                case "flaw":
+                    break;
+                case "clear":
+                    str.innerHTML = "";
+                    break;
+                case "percent":
+                    break;
+                case "div":
+                    break;
+                case "backspace":
+                    str.innerHTML = str.innerHTML.slice(0, str.innerHTML.length - 1)
+                    actionBuffer = ""
+                    buffer = ""
+                    break;
+            }
+        }
         
     }
+}
+
+function doAction(){
+    switch(actionBuffer){
+        case "mul":
+            
+            break;
+        case "minus":
+            str.innerHTML = Number(buffer) - Number(str.innerHTML)
+            buffer =  str.innerHTML
+            break;
+        case "plus":
+            break;
+        case "flaw":
+            break;
+        case "percent":
+            break;
+        case "div":
+            break;
+}
 }
 
 for(let i = 0; i < nums.length; i++){
